6c9bdd86cb68db5fcdb9ddec4ac1f4949bc36086
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-04-14 09:32:16 -0300

    Update agenda101.py

--- agenda101.py
+++ agenda101.py
@@ -1084,7 +1084,7 @@ def wpp_terca():
         resultado = cursor.fetchone()  # Recuperar o primeiro registro encontrado
 
         account_sid = 'AC753be7e4dda59387dc6b6b6cf2de74f0'
-        auth_token = '805fb5e029e18a07468372367c65cdf8'
+        auth_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
         client = Client(account_sid, auth_token)
 
         # Verificar se a busca retornou resultados
